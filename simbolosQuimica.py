228e560022067b36dcb14faf4380df0a92a53248
Soadsto <soadsto@gmail.com>
2019-01-26 18:29:28 -0600

    commit

--- simbolosQuimica.py
+++ simbolosQuimica.py
@@ -1,4 +1,5 @@
-simbolos = { "Ac", "Al", "Am", "Sb", "Ar", "As", "At", "Ba", 
+simbolos = [
+            "Ac", "Al", "Am", "Sb", "Ar", "As", "At", "Ba", 
             "Bk", "Be", "Bi", "Bh", "B", "Br", "Cd", "Ca",
             "Cf", "C", "Ce", "Cs", "Cl", "Cr", "Co", "Cu",
             "Cm", "Ds", "Db", "Dy", "Es", "Er", "Eu", "Fm", 
@@ -10,7 +11,49 @@ simbolos = { "Ac", "Al", "Am", "Sb", "Ar", "As", "At", "Ba",
             "K", "Pr", "Pm", "Pa", "Ra", "Rn", "Re", "Rh", "Rg",
             "Rb", "Ru", "Rf", "Sm", "Sc", "Sg", "Se", "Si",
             "Ag", "Na", "Sr", "S", "Ta", "Tc", "Te", "Tb", "Tl",
-            "Th", "Tm", "Sn", "Ti", "W", "U", "V", "Xe", "Y", "Zn",
-        }   
+            "Th", "Tm", "Sn", "Ti", "W", "U", "V", "Xe", "Y", "Zn"
+        ]   
 
-cadena = input("Ingresa una cadena: ")
+soluciones = 0
+
+def esSolucion(cadena):
+    return len(cadena) == 0
+
+def showSolucion(a):
+    global soluciones
+    for elem in a:
+        print(elem.capitalize(), end = ' ')
+    print()
+    soluciones = soluciones + 1
+
+def crearCandidatos(cadena, cand):
+    global simbolos
+    posibilidades = simbolos
+    for elem in posibilidades:
+        if cadena.find(elem) == 0:
+            cand.append(elem)
+    
+
+def bactrack(a, cadena):
+    if esSolucion(cadena):
+        showSolucion(a)
+    else:
+        cand = []
+        crearCandidatos(cadena, cand)
+        for c in cand:
+            a.append(c)
+            i = cadena.find(c)
+            bactrack(a, cadena[:i] + cadena[i + len(c):])
+            a.pop()
+
+def main():
+    global soluciones
+    for i, elem in enumerate(simbolos):
+        simbolos[i] = elem.casefold()
+        
+    # print(simbolos)
+    cadena =  input("Ingresa una cadena: ")
+    bactrack([], cadena)
+    print("Hay un total de ", soluciones, " soluciones.")
+    
+main()
