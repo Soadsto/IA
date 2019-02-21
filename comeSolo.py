ab93784cef3d16830df98a6203185f872b3d326c
Owl <soadsto@gmail.com>
2019-02-21 15:15:10 -0600

    commit

--- comeSolo.py
+++ comeSolo.py
@@ -1,3 +1,157 @@
-posiciones = [
-    1, 
-] 
+s = 0
+soluciones = [[0],[0,0],[0,0,0],[0,0,0,0],[0,0,0,0,0]]
+def nuevo(mat):
+    nueva = []
+    for i in range(len(mat)):
+        nueva.append(mat[i][:])
+    return nueva
+
+def buscar(matriz):
+    global soluciones
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] == 1:
+                soluciones[i][j] += 1
+                break
+
+def funcion(matriz, punto):
+    x = punto[0]
+    y = punto[1]
+    aux = nuevo(matriz)
+    global s 
+    if x-2 >= 0 and y-2 >= 0:
+        if matriz[x-2][y-2] == 1 and matriz[x-1][y-1] == 1:
+            matriz[x][y] = 1
+            matriz[x-1][y-1] = 0
+            matriz[x-2][y-2] = 0
+            suma = 0
+            for i in range(len(matriz)):
+                suma += sum(matriz[i])
+            if suma == 1:
+                s += 1
+                buscar(matriz)
+            else:
+                for i in range(len(matriz)):
+                    for j in range(len(matriz[i])):
+                        if matriz[i][j] == 0:
+                            punto[0] = i
+                            punto[1] = j
+                            funcion(nuevo(matriz),punto[:])
+            matriz = nuevo(aux)
+    if x-2 >=0 and y < len(matriz[x-2]):
+        if matriz[x-2][y] == 1 and matriz[x-1][y] == 1:
+            matriz[x][y] = 1
+            matriz[x-1][y] = 0
+            matriz[x-2][y] = 0
+            suma = 0
+            for i in range(len(matriz)):
+                suma += sum(matriz[i])
+            if suma==1:
+                s += 1
+                buscar(matriz)
+            else:
+                for i in range(len(matriz)):
+                    for j in range(len(matriz[i])):
+                        if(matriz[i][j] == 0):
+                            punto[0] = i
+                            punto[1] = j
+                            funcion(nuevo(matriz),punto[:])
+            matriz = nuevo(aux)
+    if x+2 < len(matriz):
+        if matriz[x+2][y] == 1 and matriz[x+1][y] == 1:
+            matriz[x][y] = 1
+            matriz[x+1][y] = 0
+            matriz[x+2][y] = 0
+            suma = 0
+            for i in range(len(matriz)):
+                suma += sum(matriz[i])
+            if suma==1:
+                s += 1
+                buscar(matriz)
+            else:
+                for i in range(len(matriz)):
+                    for j in range(len(matriz[i])):
+                        if(matriz[i][j] == 0):
+                            punto[0] = i
+                            punto[1] = j
+                            funcion(nuevo(matriz),punto[:])
+            matriz = nuevo(aux)
+    if x+2 < len(matriz) and y+2 < len(matriz[x+2]):
+        if matriz[x+2][y+2] == 1 and matriz[x+1][y+1] == 1:    
+            matriz[x][y] = 1
+            matriz[x+1][y+1] = 0
+            matriz[x+2][y+2] = 0
+            suma = 0
+            for i in range(len(matriz)):
+                suma += sum(matriz[i])
+            if suma==1:
+                s += 1
+                buscar(matriz)
+            else:
+                for i in range(len(matriz)):
+                    for j in range(len(matriz[i])):
+                        if(matriz[i][j] == 0):
+                            punto[0] = i
+                            punto[1] = j
+                            funcion(nuevo(matriz),punto[:])
+            matriz = nuevo(aux)
+    if y-2 >= 0:
+        if matriz[x][y-2] == 1 and matriz[x][y-1] == 1:  
+            matriz[x][y] = 1
+            matriz[x][y-1] = 0
+            matriz[x][y-2] = 0
+            suma = 0
+            for i in range(len(matriz)):
+                suma += sum(matriz[i])
+            if suma==1:
+                s += 1
+                buscar(matriz)            
+            else:
+                for i in range(len(matriz)):
+                    for j in range(len(matriz[i])):
+                        if(matriz[i][j] == 0):
+                            punto[0] = i
+                            punto[1] = j
+                            funcion(nuevo(matriz),punto[:])
+            matriz = nuevo(aux)
+    if y+2 < len(matriz[x]):
+        if matriz[x][y+2] == 1 and matriz[x][y+1] == 1:    
+            matriz[x][y] = 1
+            matriz[x][y+1] = 0
+            matriz[x][y+2] = 0
+            suma = 0
+            for i in range(len(matriz)):
+                suma += sum(matriz[i])
+            if suma==1:
+                s += 1
+                buscar(matriz)
+            else:
+                for i in range(len(matriz)):
+                    for j in range(len(matriz[i])):
+                        if(matriz[i][j] == 0):
+                            punto[0] = i
+                            punto[1] = j
+                            funcion(nuevo(matriz),punto[:])
+            matriz = nuevo(aux)
+    
+def main():
+    coordenada = []
+    coordenada.append(int(input("Renglon: "))-1)
+    coordenada.append(int(input("Columna: "))-1)
+    mat = []
+    for i in range(5):
+        fila = []
+        for j in range(i+1):
+            if i == coordenada[0] and j == coordenada[1]:
+                fila.append(0)
+            else:
+                fila.append(1)
+        mat.append(fila)    
+    funcion(nuevo(mat),coordenada[:])
+    global s
+    global soluciones
+    print("Se encontraron: "+ str(s) + " soluciones" )
+    for i in range(len(soluciones)):
+        for j in range(len(soluciones[i])):
+            print("Posicion [" + str(i+1) + "," + str(j+1) +"] = " + str(soluciones[i][j]) )
+main()
---
+++ mondriaan.py
@@ -0,0 +1,43 @@
+"""
+    Salida de un rectangulo de 4*4 = 36
+    La entrada debera ser las dimensiones del rectangulo, ya sea renglon * columna
+    La salida de manera general son la cantidad de configuraciones con recangulos de 2*1 | 1*2 dentro de un rectangulo
+    definido por el usuario
+"""
+#Sergio Téllez Ojeda
+#Inteligencia artificial 
+soluciones = 0
+def esSolucion():
+    global soluciones
+    soluciones += 1
+
+def solucion(numMov):
+    if numMov > m + n / 2:
+        esSolucion()
+    else:
+        r = 0
+        c = 0
+        while r < m and a[r][c] != 0:
+            c += 1
+            if c == n:
+                c = 0
+                r += 1
+        if r < m:
+            if (c + 1) < n and a[r][c + 1] == 0:
+                a[r][c], a[r][c + 1] = numMov, numMov
+                solucion(numMov + 1)
+                a[r][c], a[r][c + 1] = 0, 0
+            if (r + 1) < m and a[r + 1][c] == 0:
+                a[r][c], a[r + 1][c] = numMov, numMov
+                solucion(numMov + 1)
+                a[r][c], a[r + 1][c] = 0, 0
+
+m = int(input("Renglones: "))
+n = int(input("Columnas: "))
+if (m + n) % 2 == 0:
+    a = ([0 for i in range(m)] for i in range(n))
+    a = list(a)
+    solucion(1)
+else:
+    print("Soluciones = 0")
+print("Soluciones:", soluciones)
