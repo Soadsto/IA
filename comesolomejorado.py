31aa4a65ddb731e74b06f71ac2de17ee7547137b
Soadsto <soadsto@gmail.com>
2019-02-24 12:36:00 -0600

    Add files via upload

--- comesolomejorado.py
+++ comesolomejorado.py
@@ -1,57 +1,102 @@
 #Funcion para obtener el renglon dada la posicion
-def renglon(posicion):
-    r = 0
-    while (r+1)*(r+2)/2 < posicion:
-        r += 1
-    return r
+soluciones = 0
 
+def imprime(lista):
+    print(lista[0])
+    print(*lista[1:3], sep='\t')
+    print(*lista[3:6], sep='\t')
+    print(*lista[6:10], sep='\t')
+    print(*lista[10:], sep='\t')
+
+def renglon(posicion):
+    if posicion == 0: return 0
+    if posicion < 3: return 1
+    if posicion < 6: return 2
+    if posicion < 10: return 3
+    return 4 
 #Funcion para obtener la columna dada la posicion
 def columna(posicion):
-    r = 0
-    while (r+1)*(r+2)/2 < posicion: 
-        r += 1
-    return int(posicion-r*(r+1)/2) 
+    if posicion == 0: return 0
+    if posicion < 3: return (posicion-1) % 2
+    if posicion < 6: return (posicion-3) % 3
+    if posicion < 10: return (posicion-6) % 4
+    return (posicion-10) % 5
 
-def posicion(renglon, columna):
-    return int(renglon*(renglon + 1)/2 + columna)
+def posicion(r, c):
+    if c > r: return -1
+    return int(r*(r + 1)/2 + c)
 
-def es_solucion(k):
-    return k == 13
+def es_solucion(entrada):
+    print('Faltan ', entrada.count(True))
+    return entrada.count(True) < 4
 
-def crearCandidatos(a, input, cand):
+def crearCandidatos(a, entrada):
+    final, medio = [], []
     p = a[-1] #Posicion del ultimo moviento
     i, j = renglon(p), columna(p)
+    # print('Posicion: ',p)
+    # imprime(entrada)
     if j - 2 >= 0: # mov 5
-        if input[posicion(i, j-1)] and input[posicion(i, j-2)]:
-            cand.append(posicion(i, j-2))
+        m, f = posicion(i, j-1), posicion(i, j-2)
+        if entrada[m] and entrada[f]:
+            if f >= 0:
+                final.append(f)
+                medio.append(m)
+            #*********************************************
     if i - 2 >= 0 and j - 2 >= 0: #mov 3
-        if input[posicion(i-1, j-i)] and input[posicion(i-2, j-2)]:
-            cand.append(posicion(i-2, j-2))
+        m, f = posicion(i-1, j-1), posicion(i-2, j-2)
+        if entrada[m] and entrada[f]:
+            if f >= 0:
+                final.append(f)
+                medio.append(m)
+            #*********************************************
     if i - 2 >= 0: #mov 2
-        if input[posicion(i-1, j)] and input[posicion(i-2, j)]:
-            cand.append(posicion(i-2, j))
+        m, f = posicion(i-1, j), posicion(i-2, j)
+        if entrada[m] and entrada[f]:
+            if f >= 0:
+                final.append(f)
+                medio.append(m)
+            # *********************************************
     if j + 2 <= 4: #mov 6
-        if input[posicion(i, j+1)] and input[posicion(i, j+2)]:
-            cand.append(posicion(i, j+2))
+        m, f = posicion(i, j+1), posicion(i, j+2)
+        if entrada[m] and entrada[f]:
+            if f >= 0:
+                final.append(f)
+                medio.append(m)
+            #*********************************************
     if i + 2 <= 4 and j + 2 <= 4: #mov 4
-        if input[posicion(i+1, j+1)] and input[posicion(i+2, j+2)]:
-            cand.append(posicion(i+2, j+2))
+        m, f = posicion(i+1, j+1), posicion(i+2, j+2)
+        if entrada[m] and entrada[f]:
+            if f >= 0:
+                final.append(f)
+                medio.append(m)
+            #*********************************************
     if i + 2 <= 4: #mov 1 
-        if input[posicion(i+1, j)] and input[posicion(i+2, j)]: 
-            cand.append(posicion(i+2, j))            
+        m, f = posicion(i+1, j), posicion(i+2, j)
+        if entrada[m] and entrada[f]: 
+            if f >= 0:
+                final.append(f)
+                medio.append(m)
+    # print(final, '8========D', medio)
+    # print('-----------------------------------')
+    # input()
+    return final, medio
 
-def backtrack(a, k, input):
-    if es_solucion(k):
-        print (a)
+def backtrack(a, k, entrada):
+    if es_solucion(entrada):
+        global soluciones
+        soluciones += 1
+        print(a)
     else:
-        cand = []
-        crearCandidatos(a, input, cand)
+        final, medio = crearCandidatos(a, entrada)
         k += 1
-        for c in cand:
-            a.append(c)
-            input[c] = False
-            backtrack(a, k, input)
-            input[c] = True
+        for i, f in enumerate(final):
+            new_input = list(entrada)
+            new_input[a[-1]] = True
+            new_input[f] = False
+            new_input[medio[i]] = False
+            a.append(f)
+            backtrack(a, k, new_input)
             a.pop()
 
 def comesolo():
@@ -59,5 +104,7 @@ def comesolo():
     tablero = [True for i in range(15)] #Aqui acomodamos las posiciones del tablero de arriba hacia abajo
     tablero[n] = False
     backtrack([n], 0, tablero)
+    global soluciones
+    print(soluciones)
 
 comesolo()
